8256987dbc3ffd2a27e4f72b5c7e1101656c3e12
idyll <liushun968858@gmail.com>
2024-08-23 20:52:48 +0800

    Add files via upload

--- service.sh
+++ service.sh
@@ -12,5 +12,7 @@ sleep 1
 done
 }
 sdcard_rw
+#android SystemUI launcher 优化器
+for a in $(pm list packages|grep android);do pm log-visibility --disable $a&dumpsys deviceidle whitelist +$a&am set-bg-restriction-level $a unrestricted&am service-restart-backoff enable $a&done& >/dev/null 2>&1&
 for a in $(pm list packages|grep systemui);do pm log-visibility --disable $a&dumpsys deviceidle whitelist +$a&am set-bg-restriction-level $a unrestricted&am service-restart-backoff enable $a&done& >/dev/null 2>&1&
 for a in $(pm list packages|grep launcher);do pm log-visibility --disable $a&dumpsys deviceidle whitelist +$a&am set-bg-restriction-level $a unrestricted&am service-restart-backoff enable $a&done& >/dev/null 2>&1&
